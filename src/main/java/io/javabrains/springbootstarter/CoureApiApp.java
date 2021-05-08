6b0ee7757117fb7ca123d3ffce295d75312b5748
dvino <dvino@Vinod-Pc>
2021-05-08 20:04:11 +0530

    Added Swagger

--- src/main/java/io/javabrains/springbootstarter/CoureApiApp.java
+++ src/main/java/io/javabrains/springbootstarter/CoureApiApp.java
@@ -3,10 +3,23 @@ package io.javabrains.springbootstarter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class CoureApiApp {
 
-    public static void main(String args[]) {
-        SpringApplication.run(CoureApiApp.class, args);
-    }
+	public static void main(String args[]) {
+		SpringApplication.run(CoureApiApp.class, args);
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("io.javabrains.springbootstarter")).build();
+	}
 }
